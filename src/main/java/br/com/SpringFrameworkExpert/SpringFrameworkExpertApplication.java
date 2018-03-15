1c1a235c9282555bdbcadab039fa9e544dc7d638
Bruno Batista <brunno188@hotmail.com>
2018-03-15 00:24:52 -0300

    commit inicial

---
+++ src/main/java/br/com/SpringFrameworkExpert/SpringFrameworkExpertApplication.java
@@ -0,0 +1,12 @@
+package br.com.SpringFrameworkExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkExpertApplication.class, args);
+	}
+}
